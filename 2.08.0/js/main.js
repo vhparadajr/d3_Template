05ff9adc19696cf5f725cb478ad3e08255899038
vhparadajr <vhparadajr@gmail.com>
2018-08-04 15:50:05 -0400

    line chart

--- 2.08.0/js/main.js
+++ 2.08.0/js/main.js
@@ -3,7 +3,7 @@
 *    Mastering Data Visualization with D3.js
 *    2.8 - Activity: Your first visualization!
 */
-var margin = {left:100, right :10, top:10, bottom:100}
+var margin = {left:100, right:10, top:10, bottom:100}
 var width = 600 - margin.left - margin.right
 var height = 400 - margin.top - margin.bottom
 
@@ -14,60 +14,51 @@ var svg = d3.select("#chart-area").append("svg")
 var g =svg.append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-d3.json("data/buildings.json").then(function(data){
+var parseTime = d3.timeParse("%Y-%m-%d%H:%M:%S");
+  console.log(parseTime("2016-09-1013:53:24"))
+// var parseTime = d3.timeParse("%H:%M:%S");
+//   console.log(parseTime("13:53:24"))
+
+d3.json("data/revenue.json").then(function(data){
   data.forEach((d) =>{
-    d.height = +d.height
+    d.updated = parseTime(d.updated);
+    d.revenuePerDay = parseFloat(d.revenuePerDay.slice(1));
   })
 
+  var x = d3.scaleTime()
+  .range([0,width])
 
-  var x = d3.scaleBand()
-  .domain(data.map((d) =>{
-    return d.name
-  }))
-  .range([0,height])
-  .paddingInner(0.3)
-  .paddingOuter(0.3)
 
   var y = d3.scaleLinear()
-    .domain([0, d3.max(data, (d) =>{
-      return d.height
-    })])
-    .range([0, height])
-
-  var xAxisCall = d3.axisBottom(x)
-  g.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "Translate(0, " + height + ")")
-    .call(xAxisCall)
-
-  var yAxisCall = d3.axisLeft(y)
-  g.append("g")
-    .attr("class", "y axis")
-    .call(yAxisCall)
+    .range([height, 0])
 
 
-  var rect = g.selectAll("rect")
-    .data(data)
-    .enter()
-    .append('rect')
-      .attr('x', (d, i) =>{
-        return x(d.name)
-      })
-      .attr('y', 20)
-      .attr('width', x.bandwidth)
-      .attr('height', (d) =>{
-        return y(d.height)
-      })
-      .attr('rx', 0)
-      .attr('ry', 0)
-      .attr('stroke', 'black')
-      .style('fill', (d) =>{
-        if(d.name == "Lotte World Tower"){
-          return "green"
-        }else{
-          return "red"
-        }
-      });
+  // define the line
+  var valueline = d3.line()
+    .x(function(d) { return x(d.updated); })
+    .y(function(d) { return y(d.revenuePerDay); });
+
+  // Scale the range of the data
+  x.domain(d3.extent(data, function(d) { return d.updated; }));
+  y.domain([0, d3.max(data, function(d) { return d.revenuePerDay; })]);
+
+
+  var xAxisCall = d3.axisBottom(x);
+    g.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0, " + height + ")")
+      .call(xAxisCall);
+
+  var yAxisCall = d3.axisLeft(y);
+    g.append("g")
+      .attr("class", "y axis")
+      .call(yAxisCall);
+
+    // Add the valueline path.
+   g.append("path")
+       .data([data])
+       .attr("class", "line")
+       .attr("d", valueline);
 }).catch((error) =>{
   console.log(error)
 })
